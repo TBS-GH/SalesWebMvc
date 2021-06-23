bed7770ed4622363d6c61e3e2317212f665a28e1
Thiago Santana <thiagoGitHub007@gmail.com>
2021-06-23 15:51:07 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    //aqui é uma classe normal dentro da pasta models
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
